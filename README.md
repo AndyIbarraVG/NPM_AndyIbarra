65e528f953135ffe3b06a45e166ea1861f52ed3b
AndyIbarraVG <AndyIbarraVG@outlook.com>
2024-02-26 09:50:05 -0600

    Se agregó un modulo

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # NPM_AndyIbarra
+
+Prueba de Modulo NPM
+
+Ejercicio de creacion de modulo de Javascript y publicacion en el registro NPM.
+
+Instalacion del modulo
+
+```bash
+$npm install npm_andyibarra
+```
+Como se utiliza este modulo
+```js
+const npm_andyibarra = require('npm_andyibarra')
+
+ObtienePokemon();
---
+++ modulopkm.js
@@ -0,0 +1,24 @@
+let pokemons = [
+    { nombre: "Bulbasaur", imagen: "https://img.pokemondb.net/sprites/x-y/normal/bulbasaur.png" },
+    { nombre: "Ivysaur", imagen: "https://img.pokemondb.net/sprites/x-y/normal/ivysaur.png" },
+    { nombre: "Venusaur", imagen: "https://img.pokemondb.net/sprites/x-y/normal/venusaur.png" },
+    { nombre: "Charmander", imagen: "https://img.pokemondb.net/sprites/x-y/normal/charmander.png" },
+    { nombre: "Charmeleon", imagen: "https://img.pokemondb.net/sprites/x-y/normal/charmeleon.png" },
+    { nombre: "Charizard", imagen: "https://img.pokemondb.net/sprites/x-y/normal/charizard.png" },
+    { nombre: "Squirtle", imagen: "https://img.pokemondb.net/sprites/x-y/normal/squirtle.png" },
+    { nombre: "Wartortle", imagen: "https://img.pokemondb.net/sprites/x-y/normal/wartortle.png" },
+    { nombre: "Blastoise", imagen: "https://img.pokemondb.net/sprites/x-y/normal/blastoise.png" },
+    { nombre: "Caterpie", imagen: "https://img.pokemondb.net/sprites/x-y/normal/caterpie.png" },
+    { nombre: "Metapod", imagen: "https://img.pokemondb.net/sprites/x-y/normal/metapod.png" },
+    { nombre: "Butterfree", imagen: "https://img.pokemondb.net/sprites/x-y/normal/butterfree.png" },
+    { nombre: "Weedle", imagen: "https://img.pokemondb.net/sprites/x-y/normal/weedle.png" },
+    { nombre: "Kakuna", imagen: "https://img.pokemondb.net/sprites/x-y/normal/kakuna.png" },
+    { nombre: "Beedrill", imagen: "https://img.pokemondb.net/sprites/x-y/normal/beedrill.png" },
+    { nombre: "Pidgey", imagen: "https://img.pokemondb.net/sprites/x-y/normal/pidgey.png" },
+    { nombre: "Pidgeotto", imagen: "https://img.pokemondb.net/sprites/x-y/normal/pidgeotto.png" },
+    { nombre: "Pidgeot", imagen: "https://img.pokemondb.net/sprites/x-y/normal/pidgeot.png" },
+];
+
+export function ObtienePokemon() {
+    return pokemons[Math.floor(Math.random() * pokemons.length)];
+}
